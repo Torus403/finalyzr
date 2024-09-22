18bbe3f1319924c2e207dd7812d11e579987cb04
Jack McNish <jackmcnish333@gmail.com>
2024-09-22 18:18:58 +0100

    improve function name

--- app/core/security.py
+++ app/core/security.py
@@ -23,5 +23,5 @@ def verify_password(plain_password: str, hashed_password: str) -> bool:
     return pwd_context.verify(plain_password, hashed_password)
 
 
-def get_password_hash(password: str) -> str:
+def hash_password(password: str) -> str:
     return pwd_context.hash(password)
