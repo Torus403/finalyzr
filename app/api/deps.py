d5e9a5213889ab116e17e3cce9967c939eb2fc06
Jack McNish <jackmcnish333@gmail.com>
2024-09-22 18:17:38 +0100

    remove reference to is_active

--- app/api/deps.py
+++ app/api/deps.py
@@ -15,7 +15,7 @@ from app.schemas.login import TokenPayload
 from app.models.users import User
 
 reusable_oauth2 = OAuth2PasswordBearer(
-    tokenUrl=f"{settings.API_V1_STR}/login/access-token"
+    tokenUrl=f"{settings.API_V1_STR}/login/"
 )
 
 
@@ -39,18 +39,18 @@ def get_current_user(session: SessionDep, token: TokenDep) -> User:
             status_code=status.HTTP_403_FORBIDDEN,
             detail="Could not validate credentials",
         )
+
     user = session.get(User, token_data.sub)
     if not user:
         raise HTTPException(status_code=404, detail="User not found")
-    if not user.is_active:
-        raise HTTPException(status_code=400, detail="Inactive user")
+
     return user
 
 
 CurrentUser = Annotated[User, Depends(get_current_user)]
 
 
-def get_current_active_superuser(current_user: CurrentUser) -> User:
+def get_current_superuser(current_user: CurrentUser) -> User:
     if not current_user.is_superuser:
         raise HTTPException(
             status_code=403, detail="The user doesn't have enough privileges"
