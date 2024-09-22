0ebb0a1e15c7ce3d93f0f8ff990b616b91622a03
Jack McNish <jackmcnish333@gmail.com>
2024-09-22 15:59:14 +0100

    make user table use UUIDs

--- app/models/users.py
+++ app/models/users.py
@@ -1,12 +1,13 @@
-from sqlalchemy import Column, Integer, String, Boolean
-
+import uuid
+from sqlalchemy import Column, String, Boolean
+from sqlalchemy.dialects.mysql import CHAR
 from app.core.db import Base
 
 
 class User(Base):
     __tablename__ = "users"
 
-    id = Column(Integer, primary_key=True, index=True)
+    id = Column(CHAR(36), primary_key=True, index=True, default=lambda: str(uuid.uuid4()))
     email = Column(String(255), unique=True, index=True, nullable=False)
     password = Column(String(255), nullable=False)
     is_superuser = Column(Boolean, nullable=False, default=False)
