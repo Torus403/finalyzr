66aa1312bdb5c154689810337ace3ad51cebabb8
Jack McNish <jackmcnish333@gmail.com>
2024-09-29 19:23:04 +0100

    add rate-limiting demo

--- app/main.py
+++ app/main.py
@@ -1,4 +1,8 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
+from slowapi import Limiter
+from slowapi.errors import RateLimitExceeded
+from slowapi.util import get_remote_address
+from starlette.responses import JSONResponse
 
 from app.api.main import api_router
 from app.core.config import settings
@@ -9,6 +13,10 @@ logging_settings.setup()
 
 from app.core.middleware import log_requests, add_request_id
 
+
+limiter = Limiter(key_func=get_remote_address)
+
+
 app = FastAPI(
     title=settings.PROJECT_NAME,
     description="Welcome to Finalyzr's API documentation! Here you will be able to discover all the ways you can "
@@ -16,12 +24,24 @@ app = FastAPI(
     openapi_url=f"{settings.API_V1_STR}/openapi.json",
 )
 
+app.state.limiter = limiter
+
+
 app.middleware("http")(log_requests)
 app.middleware("http")(add_request_id)
 
 
+@app.exception_handler(RateLimitExceeded)
+async def rate_limit_handler(request: Request, exc: RateLimitExceeded):
+    return JSONResponse(
+        status_code=429,
+        content={"detail": "Rate limit exceeded. Try again later."},
+    )
+
+
 @app.get("/")
-async def root():
+@limiter.limit("50/minute")
+async def root(request: Request):
     return "Hello, World!"
 
 
